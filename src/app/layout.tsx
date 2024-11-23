cc8545285895a03e6e3e835220610844e0330ffd
salah137 <boksaloh@gmail.com>
2024-11-23 12:40:18 +0100

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "CAD",
@@ -14,7 +14,9 @@ export default function RootLayout({
   }>) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>{children}
+          <Analytics/>
+        </body>
       </html>
     );
   }
